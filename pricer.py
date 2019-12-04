98e7ab3b78731a14bbc2274ab60240b0b4fb65f4
rchen2018 <rec43@duke.edu>
2019-12-04 01:59:43 -0500

    search works, committing before I delete code comments

--- pricer.py
+++ pricer.py
@@ -1,13 +1,24 @@
-import selenium
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get('https://us.myprotein.com/')
 
+# need to maximize window to standardize results (errors occur in minimized window)
+# driver.maximize_window()
+# driver.find_element_by_class_name('headerSearch_toggleForm').click()  PROBABLY DONT NEED
+
 # search for impact whey protein
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'search')))
 search = driver.find_element_by_name('search')
+
 search.send_keys('impact whey protein')
+search.send_keys(Keys.RETURN)
 
 # find the flavor and select it
 # TODO fix this so that it routes to select tag
