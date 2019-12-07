9c1f4029e83a177cad0807f3a3ff7e75e164aa3e
rchen2018 <rec43@duke.edu>
2019-12-07 18:01:58 -0500

    correctly adds to cart and goes to checkout

--- pricer.py
+++ pricer.py
@@ -6,29 +6,34 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 driver.maximize_window()
 driver.get('https://us.myprotein.com/')
 
-# need to maximize window to standardize results (errors occur in minimized window)
-# driver.maximize_window()
-# driver.find_element_by_class_name('headerSearch_toggleForm').click()  PROBABLY DONT NEED
-
-# search for impact whey protein
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'search')))
+# find the search bar
+wait.until(EC.presence_of_element_located((By.NAME, 'search')))
 search = driver.find_element_by_name('search')
 
+# search for impact whey protein
 search.send_keys('impact whey protein')
 search.send_keys(Keys.RETURN)
 
 # find the flavor and select it
-# TODO fix this so that it routes to select tag
-strawberry_cream = driver.find_element_by_xpath("//option[contains(text(), 'Strawberry Cream')]")
-# strawberry_cream = driver.find_element_by_xpath("//option[@value='2415']")
-Select(strawberry_cream).select_by_visible_text('Strawberry Cream')
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'athenaProductVariations_dropdown')))
+flavor = driver.find_element_by_class_name('athenaProductVariations_dropdown')
+
+Select(flavor).select_by_visible_text('Strawberry Cream')
 
 # select the 11 lb bag
-driver.find_element_by_css_selector("input[value='16226']").click()
+wait.until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), '11 lb')]")))
+driver.find_element_by_xpath("//*[contains(text(), '11 lb')]").click()  # TODO sometimes doesn't register before adding to cart
+
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'productAddToBasket')))
+driver.find_element_by_class_name('productAddToBasket').click()
+
+wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Checkout')))
+driver.find_element_by_link_text('Checkout').click()
 
 
 if __name__ == "__main__":
-    print("asdf")
+    print("no errors!")
