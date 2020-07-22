b67c521810d57afb523947625ec9871f220371db
Robert Chen <rec43@duke.edu>
2020-07-22 15:50:08 -0400

    refactor code into functions

--- pricer.py
+++ pricer.py
@@ -9,61 +9,82 @@ from selenium.common.exceptions import StaleElementReferenceException, TimeoutEx
     ElementClickInterceptedException
 import time
 
+STRAWBERRY_11LB_SELECTOR = "div[data-variation-container = 'athenaProductVariations']"
+STRAWBERRY_11LB_VALUE = 'sports-nutrition/impact-whey-protein-strawberry-cream-11lb/10852509.html'
+
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 2)
 
 driver.maximize_window()
 driver.get('https://us.myprotein.com/')
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+# soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-# find the search bar
-search = wait.until(EC.presence_of_element_located((By.NAME, 'search')))
 
-# search for impact whey protein
-search.send_keys('impact whey protein')
-search.send_keys(Keys.RETURN)
+def main():
+    search_for_protein()
 
-notWorking = True
-while notWorking:
-    try:
-        # find the flavor and select it
-        flavor = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'athenaProductVariations_dropdown')))
-        Select(flavor).select_by_visible_text('Strawberry Cream')
+    not_working = True
+    while not_working:
+        try:
+            select_strawberry_11lb()
 
-        # select the 11 lb bag
-        element = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), '11 lb')]")))
-        element.click()
-        time.sleep(2)  # wait 2 seconds to allow page to catch up
+            # check that product is 11 pound strawberry cream
+            strawberry_11 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, STRAWBERRY_11LB_SELECTOR)))
+            if strawberry_11.get_attribute('data-information-url') != STRAWBERRY_11LB_VALUE:
+                continue
 
-        # check that product is 11 pound strawberry cream
-        strawberry_11 = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-variation-container = 'athenaProductVariations']")))
-        if strawberry_11.get_attribute('data-information-url') != 'sports-nutrition/impact-whey-protein-strawberry-cream-11lb/10852509.html':
+            not_working = add_and_go_to_cart(not_working)
+
+        except (StaleElementReferenceException, TimeoutException):
+            driver.refresh()
+
+        # Ad popup blocks window
+        except ElementClickInterceptedException:
+            close = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'emailReengagement_close_button')))
+            close.click()
             continue
 
-        # add to cart
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'productAddToBasket')))
-        driver.find_element_by_class_name('productAddToBasket').click()
-        time.sleep(2)  # wait 2 seconds to allow page to catch up
+    # sale = soup.find(class_='countDownTimer_link').text
+    # print(sale)
 
-        product = driver.find_element_by_class_name('athenaAddedToBasketModal_itemName')
-        if "11lb" in product.text and "Strawberry Cream" in product.text:
-            notWorking = False
-            # go to cart
-            wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Checkout')))
-            driver.find_element_by_link_text('Checkout').click()
 
-    except (StaleElementReferenceException, TimeoutException) as e:
-        print(e)
-        driver.refresh()
+def search_for_protein():
+    # find the search bar
+    search = wait.until(EC.presence_of_element_located((By.NAME, 'search')))
 
-    except ElementClickInterceptedException:
-        close = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'emailReengagement_close_button')))
-        close.click()
-        continue
+    # search for impact whey protein
+    search.send_keys('impact whey protein')
+    search.send_keys(Keys.RETURN)
 
-# sale = soup.find(class_='countDownTimer_link').text
-# print(sale)
+
+def select_strawberry_11lb():
+    # find the flavor and select it
+    flavor = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'athenaProductVariations_dropdown')))
+    Select(flavor).select_by_visible_text('Strawberry Cream')
+
+    # select the 11 lb bag
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), '11 lb')]")))
+    element.click()
+    time.sleep(2)  # wait 2 seconds to allow page to catch up
+
+
+def add_and_go_to_cart(not_working):
+    # add to cart
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'productAddToBasket')))
+    driver.find_element_by_class_name('productAddToBasket').click()
+    time.sleep(2)  # wait 2 seconds to allow page to catch up
+
+    product = driver.find_element_by_class_name('athenaAddedToBasketModal_itemName')
+    if "11lb" in product.text and "Strawberry Cream" in product.text:
+        not_working = False
+
+        # go to cart
+        wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Checkout')))
+        driver.find_element_by_link_text('Checkout').click()
+
+    return not_working
+
 
 if __name__ == "__main__":
-    print("success")
+    main()
